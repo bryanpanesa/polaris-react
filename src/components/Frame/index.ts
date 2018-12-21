bca5243cac99fb2b622c9853f0b2ed11971b91d7
Dan Rosenthal <dan.rosenthal@shopify.com>
2018-12-21 08:48:24 -0500

    export necessary pieces from Frame

--- src/components/Frame/index.ts
+++ src/components/Frame/index.ts
@@ -2,13 +2,8 @@ import Frame from './Frame';
 
 export {Props} from './Frame';
 
-export {DEFAULT_TOAST_DURATION} from './components';
+export {DEFAULT_TOAST_DURATION, Consumer} from './components';
 
-export {
-  ContextualSaveBarProps,
-  FrameContext,
-  frameContextTypes,
-  ToastProps,
-} from './types';
+export {ContextualSaveBarProps, FrameContext, ToastProps} from './types';
 
 export default Frame;
